542c992360ff7c417df05219c9fb46f3c831bb23
Dmintriy Ivanov <shooterbot@gmail.com>
2022-11-15 13:43:24 +0300

    Added bytes to float casting

---
+++ src/utility/cast.go
@@ -0,0 +1,13 @@
+package utility
+
+import (
+	"bytes"
+	"encoding/binary"
+)
+
+func BytesToFloat64(data []byte) float64 {
+	var res float64
+	buf := bytes.NewReader(data)
+	_ = binary.Read(buf, binary.LittleEndian, &res)
+	return res
+}
