b27b762adf1324018a25c578cc4b3b7098345525
Dmintriy Ivanov <shooterbot@gmail.com>
2022-11-15 18:37:15 +0300

    Added reservation usecases

--- src/usecases/balance.go
+++ src/usecases/balance.go
@@ -3,4 +3,5 @@ package usecases
 type IBalanceUsecase interface {
 	GetByUserId(id int) (float64, error)
 	AddByUserId(id int, amount float64) error
+	AddReservation(userId int, serviceId int, amount float64) error
 }
--- src/usecases/uc_implementation/balance.go
+++ src/usecases/uc_implementation/balance.go
@@ -23,3 +23,26 @@ func (bc *BalanceUsecases) AddByUserId(id int, amount float64) error {
 	}
 	return bc.br.AddByUserId(id, amount)
 }
+
+func (bc *BalanceUsecases) AddReservation(userId int, serviceId int, amount float64) error {
+	current, err := bc.br.GetByUserId(userId)
+	if err != nil {
+		return err
+	}
+	if current < amount {
+		return errors.New("User balance is too low")
+	}
+
+	err = bc.br.Withdraw(userId, amount)
+	if err != nil {
+		return err
+	}
+	err = bc.br.AddReservation(userId, userId, amount)
+	if err != nil {
+		// Must return money if reservation has failed
+		// Creating new local err to return the actual error message
+		for err := errors.New(""); err != nil; err = bc.br.AddByUserId(userId, amount) {
+		}
+	}
+	return err
+}
