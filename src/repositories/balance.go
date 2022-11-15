c44b264543960ea7b289a219d939104b9efaa944
Dmintriy Ivanov <shooterbot@gmail.com>
2022-11-15 19:37:54 +0300

    Added reservation commiting repo

--- src/repositories/balance.go
+++ src/repositories/balance.go
@@ -4,5 +4,7 @@ type IBalanceRepository interface {
 	GetByUserId(id int) (float64, error)
 	AddByUserId(id int, amount float64) error
 	Withdraw(id int, amount float64) error
-	AddReservation(userId int, serviceId int, amount float64) error
+	AddReservation(userId int, orderId int, serviceId int, amount float64) error
+	CommitReservation(userId int, orderId int, serviceId int) error
+	GetReserved(orderId int, serviceId int) (float64, error)
 }
--- src/repositories/repo_implementation/balance.go
+++ src/repositories/repo_implementation/balance.go
@@ -12,7 +12,13 @@ const (
 	AddAmountByUserId    = `update balances set amount = amount + $2 where user_id = $1;`
 	CreateBalance        = `insert into balances(user_id, amount) values($1, $2);`
 	ReduceAmountByUserId = `update balances set amount = amount - $2 where user_id = $1;`
-	Reserve              = `insert into reservations(user_id, service_id, amount) values($1, $2, $3);`
+	InsertReservation    = `insert into reservations(user_id, service_id, order_id, amount) values($1, $2, $3, $4);`
+	selectReserved       = `select amount from reservations where order_id = $1 and service_id = $2;`
+
+	// DeleteReservation Вообще, для удаления записи о резерве достаточно только ID заказа, он должен быть уникальным
+	// (и, например, ссылаться на заказ, в ктором уже хранится ID услуги), иначе в нем нет смысла. Но, раз по ТЗ
+	// остальные параметры тоже передаются в запросе - можно сделать проверку и по ним
+	DeleteReservation = `delete from reservations where user_id = $1 and service_id = $2 and order_id = $3;`
 )
 
 type BalanceRepository struct {
@@ -58,8 +64,31 @@ func (br *BalanceRepository) Withdraw(id int, amount float64) error {
 	return err
 }
 
-func (br *BalanceRepository) AddReservation(userId int, serviceId int, amount float64) error {
-	affected, err := br.db.Exec(Reserve, userId, serviceId, amount)
+func (br *BalanceRepository) AddReservation(userId, orderId, serviceId int, amount float64) error {
+	affected, err := br.db.Exec(InsertReservation, userId, serviceId, orderId, amount)
+	if err != nil && affected == 0 {
+		err = errors.New("Error while updating user reservation")
+	}
+	return err
+}
+
+func (br *BalanceRepository) GetReserved(orderId int, serviceId int) (float64, error) {
+	var res float64
+	data, err := br.db.Query(selectReserved, orderId, serviceId)
+	if err != nil {
+		fmt.Printf("Failed to get reserved amount from db\n")
+	} else if len(data) == 1 {
+		res = utility.BytesToFloat64(data[0][0])
+	} else {
+		err = errors.New("Could not find a user matching the given id")
+		res = -1
+	}
+
+	return res, err
+}
+
+func (br *BalanceRepository) CommitReservation(userId, orderId, serviceId int) error {
+	affected, err := br.db.Exec(DeleteReservation, userId, serviceId, orderId)
 	if err != nil && affected == 0 {
 		err = errors.New("Error while updating user reservation")
 	}
