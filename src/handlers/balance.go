32eca78cdcf65abdd41efb224994552bb682aa0e
Dmintriy Ivanov <shooterbot@gmail.com>
2022-11-15 18:36:56 +0300

    Added reserve handler

--- src/handlers/balance.go
+++ src/handlers/balance.go
@@ -29,6 +29,7 @@ func (bh *BalanceHandlers) GetByUserId(w http.ResponseWriter, r *http.Request) {
 	query := r.URL.Query()
 	strId, present := query["userId"]
 	if !present || len(strId) != 1 {
+		// Received none or several values, error
 		fmt.Println("Received a wrong query parameter for GetByUserId")
 		http.Error(w, "Failed to get user balance: wrong query parameter", http.StatusBadRequest)
 		return
@@ -88,7 +89,7 @@ func (bh *BalanceHandlers) AddByUserId(w http.ResponseWriter, r *http.Request) {
 	amount, err := strconv.ParseFloat(strAmount[0], 64)
 	if err != nil {
 		fmt.Println("Received an invalid query parameter for AddByUserId")
-		http.Error(w, "Failed to get user balance: invalid query parameter (amount must be integer)", http.StatusBadRequest)
+		http.Error(w, "Failed to get user balance: invalid query parameter (amount must be a number)", http.StatusBadRequest)
 		return
 	}
 
@@ -102,4 +103,48 @@ func (bh *BalanceHandlers) AddByUserId(w http.ResponseWriter, r *http.Request) {
 
 	// Completing request
 	w.WriteHeader(http.StatusOK)
+}
+
+func (bh *BalanceHandlers) Reserve(w http.ResponseWriter, r *http.Request) {
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			fmt.Println("Failed to close response body")
+		}
+	}(r.Body)
+
+	// Acquiring parameters
+	query := r.URL.Query()
+	strUserId, userIdPresent := query["userId"]
+	strServiceId, serviceIdPresent := query["serviceId"]
+	strAmount, amountPresent := query["amount"]
+	if !(userIdPresent && serviceIdPresent && amountPresent) ||
+		len(strUserId) != 1 || len(strAmount) != 1 || len(strServiceId) != 1 {
+		fmt.Println("Received a wrong query parameter for AddByUserId")
+		http.Error(w, "Failed to update user balance: wrong query parameter", http.StatusBadRequest)
+		return
+	}
+
+	// Validating parameters
+	userId, err := strconv.Atoi(strUserId[0])
+	if err != nil {
+		fmt.Println("Received an invalid query parameter for AddReservation")
+		http.Error(w, "Failed to get user balance: invalid query parameter (id must be integer)", http.StatusBadRequest)
+		return
+	}
+	serviceId, err := strconv.Atoi(strUserId[0])
+	if err != nil {
+		fmt.Println("Received an invalid query parameter for AddReservation")
+		http.Error(w, "Failed to get user balance: invalid query parameter (id must be integer)", http.StatusBadRequest)
+		return
+	}
+	amount, err := strconv.ParseFloat(strAmount[0], 64)
+	if err != nil {
+		fmt.Println("Received an invalid query parameter for AddReservation")
+		http.Error(w, "Failed to get user balance: invalid query parameter (amount must be a number)", http.StatusBadRequest)
+		return
+	}
+
+	err = bh.bc.AddReservation(userId, serviceId, amount)
+
 }
