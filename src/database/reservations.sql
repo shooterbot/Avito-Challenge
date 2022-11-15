2c8762ab66630d2c5c3a93b0d7a72e44cf4561d4
Dmintriy Ivanov <shooterbot@gmail.com>
2022-11-15 19:16:51 +0300

    Added order Id as a unique reservation paramter (otherwise it is useless)

--- src/database/reservations.sql
+++ src/database/reservations.sql
@@ -3,6 +3,7 @@ create table reservations
 (
 	id serial primary key,
 	user_id int not null,
-	service_id int not null, 
+	service_id int not null,
+	order_id int not null unique,
 	amount float not null
 );
