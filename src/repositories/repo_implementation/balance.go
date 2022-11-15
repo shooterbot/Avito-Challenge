f1fe0f9b001f0cc2cc8a1ae08c8396cb5a985bf9
Dmintriy Ivanov <shooterbot@gmail.com>
2022-11-15 14:15:10 +0300

    Added balance replenishment

--- src/repositories/repo_implementation/balance.go
+++ src/repositories/repo_implementation/balance.go
@@ -3,11 +3,13 @@ package repo_implementation
 import (
 	"Avito-Challenge/src/database/pgdb"
 	"Avito-Challenge/src/utility"
+	"errors"
 	"fmt"
 )
 
 const (
 	selectAmountByUserId = `select amount from balances where user_id = $1;`
+	AddAmountByUserId    = `update balances set amount = amount + $2 where user_id = $1;`
 )
 
 type BalanceRepository struct {
@@ -15,12 +17,24 @@ type BalanceRepository struct {
 }
 
 func (br *BalanceRepository) GetByUserId(id int) (float64, error) {
+	var res float64
 	data, err := br.db.Query(selectAmountByUserId, id)
 	if err != nil {
 		fmt.Printf("Failed to get balance from db\n")
+	} else if len(data) == 1 {
+		res = utility.BytesToFloat64(data[0][0])
+	} else {
+		err = errors.New("Could not find a user matching the given id")
+		res = -1
 	}
 
-	res := utility.BytesToFloat64(data[0][0])
-
 	return res, err
+}
+
+func (br *BalanceRepository) AddByUserId(id int, amount float64) error {
+	affected, err := br.db.Exec(AddAmountByUserId, id, amount)
+	if err != nil && affected == 0 {
+		err = errors.New("Could not find a user matching the given id")
+	}
+	return err
 }
--- src/usecases/uc_implementation/balance.go
+++ src/usecases/uc_implementation/balance.go
@@ -1,5 +1,10 @@
 package uc_implementation
 
+import (
+	"Avito-Challenge/src/repositories"
+	"errors"
+)
+
 type BalanceUsecases struct {
 	br repositories.IBalanceRepository
 }
@@ -10,4 +15,11 @@ func NewBalanceUsecases(br repositories.IBalanceRepository) *BalanceUsecases {
 
 func (bc *BalanceUsecases) GetByUserId(id int) (float64, error) {
 	return bc.br.GetByUserId(id)
+}
+
+func (bc *BalanceUsecases) AddByUserId(id int, amount float64) error {
+	if amount < 0 {
+		return errors.New("Wrong parameter: amount must be positive")
+	}
+	return bc.br.AddByUserId(id, amount)
 }
