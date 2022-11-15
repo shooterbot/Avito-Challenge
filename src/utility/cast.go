a77088fd6f2dbea1762f3b321599e32b08b3d070
Dmintriy Ivanov <shooterbot@gmail.com>
2022-11-16 02:25:21 +0300

    Fixed cast bytes order

--- src/utility/cast.go
+++ src/utility/cast.go
@@ -8,6 +8,6 @@ import (
 func BytesToFloat64(data []byte) float64 {
 	var res float64
 	buf := bytes.NewReader(data)
-	_ = binary.Read(buf, binary.LittleEndian, &res)
+	_ = binary.Read(buf, binary.BigEndian, &res)
 	return res
 }
