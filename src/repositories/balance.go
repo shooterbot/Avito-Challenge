6183138da17356711c89822cb6dae71302774215
Dmintriy Ivanov <shooterbot@gmail.com>
2022-11-15 14:14:54 +0300

    Fixed amount type

--- src/repositories/balance.go
+++ src/repositories/balance.go
@@ -2,4 +2,5 @@ package repositories
 
 type IBalanceRepository interface {
 	GetByUserId(id int) (float64, error)
+	AddByUserId(id int, amount float64) error
 }
--- src/usecases/balance.go
+++ src/usecases/balance.go
@@ -2,4 +2,5 @@ package usecases
 
 type IBalanceUsecase interface {
 	GetByUserId(id int) (float64, error)
+	AddByUserId(id int, amount float64) error
 }
