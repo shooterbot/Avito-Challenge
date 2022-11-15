1a78ced30d02b1b74399befcbdc7d5d111eef5da
Dmintriy Ivanov <shooterbot@gmail.com>
2022-11-15 18:37:24 +0300

    Added reservation repos

--- src/repositories/balance.go
+++ src/repositories/balance.go
@@ -3,4 +3,6 @@ package repositories
 type IBalanceRepository interface {
 	GetByUserId(id int) (float64, error)
 	AddByUserId(id int, amount float64) error
+	Withdraw(id int, amount float64) error
+	AddReservation(userId int, serviceId int, amount float64) error
 }
--- src/repositories/repo_implementation/balance.go
+++ src/repositories/repo_implementation/balance.go
@@ -10,6 +10,9 @@ import (
 const (
 	selectAmountByUserId = `select amount from balances where user_id = $1;`
 	AddAmountByUserId    = `update balances set amount = amount + $2 where user_id = $1;`
+	CreateBalance        = `insert into balances(user_id, amount) values($1, $2);`
+	ReduceAmountByUserId = `update balances set amount = amount - $2 where user_id = $1;`
+	Reserve              = `insert into reservations(user_id, service_id, amount) values($1, $2, $3);`
 )
 
 type BalanceRepository struct {
@@ -32,9 +35,33 @@ func (br *BalanceRepository) GetByUserId(id int) (float64, error) {
 }
 
 func (br *BalanceRepository) AddByUserId(id int, amount float64) error {
-	affected, err := br.db.Exec(AddAmountByUserId, id, amount)
+	data, err := br.db.Query(selectAmountByUserId, id)
+	if err != nil {
+		return errors.New("Error while getting user amount")
+	}
+	if len(data) == 0 {
+		_, err = br.db.Exec(CreateBalance, id, amount)
+	} else {
+		_, err = br.db.Exec(AddAmountByUserId, id, amount)
+	}
+	if err != nil {
+		err = errors.New("Error while updating user amount")
+	}
+	return err
+}
+
+func (br *BalanceRepository) Withdraw(id int, amount float64) error {
+	affected, err := br.db.Exec(ReduceAmountByUserId, id, amount)
 	if err != nil && affected == 0 {
-		err = errors.New("Could not find a user matching the given id")
+		err = errors.New("Error while updating user amount")
+	}
+	return err
+}
+
+func (br *BalanceRepository) AddReservation(userId int, serviceId int, amount float64) error {
+	affected, err := br.db.Exec(Reserve, userId, serviceId, amount)
+	if err != nil && affected == 0 {
+		err = errors.New("Error while updating user reservation")
 	}
 	return err
 }
